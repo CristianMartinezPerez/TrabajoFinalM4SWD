b0ca2dd7f7cba36630224c23e545d2d53f3dd1b8
Cristian Martinez <cristian.martinez.perez@gmail.com>
2021-01-30 21:31:30 -0300

    test son 4

--- src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
@@ -9,6 +9,9 @@ import com.devops.dxc.devops.model.Util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.runners.Parameterized.Parameter;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 
@@ -20,22 +23,47 @@ class DevopsApplicationTests {
 		 return;
 	 }
 
-    @DisplayName("ahorro = 5000000, sueldo 1000000")
-	@Test
-	void Retirar150UF() {
+	 @ParameterizedTest(name = "ahorro = {0} tiene fondos ")
+	 @CsvSource({
+			 "5000000"
+	 })
+	 void PoseeFondo(int ahorro) {
+		assertTrue("Error en ahorro", ahorro > 0);
+		 return;
+	 }
 
-		Dxc res = new Dxc(5000000, 1000000);
+	 @ParameterizedTest(name = "ahorro = {0} , sueldo {1}, retiro saldo ")
+	 @CsvSource({
+			 "900000, 1000000"
+	 })
+	 public void RetirarSaldo(int ahorro,int sueldo) {
+ 
+		 Dxc res = new Dxc(ahorro, sueldo);
+		 int mi10 = res.getDxc();
+ 
+		 assertEquals(ahorro, mi10);
+		 return;
+	 }
+
+	@ParameterizedTest(name = "ahorro = {0} , sueldo {1}, retiro {2} UF ")
+	@CsvSource({
+			"5000000, 1000000, 150"
+	})
+	public void Retirar150UF(int ahorro,int sueldo, int retiroUF) {
+
+		Dxc res = new Dxc(ahorro, sueldo);
 		int mi10 = res.getDxc();
-
-		int esperado = 150* Util.getUf();
+		int esperado = retiroUF* Util.getUf();
 
 		assertEquals(esperado, mi10);
 		return;
 	}
 
-	@DisplayName("ahorro = 6000000, sueldo 2500000, paga Impuesto")
-	@Test
-	void PagaImpuesoCaso1() {
+	@ParameterizedTest(name = "ahorro = {0} , sueldo {1}, paga Impuesto ")
+	@CsvSource({
+			"6000000, 2500000"
+	})
+	public void PagaImpuesoCaso1() {
 
 		Dxc res = new Dxc(6000000, 2500000);
 		int mi10 = res.getDxc();
